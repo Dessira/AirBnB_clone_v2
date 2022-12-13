fb2800265f7b4ce48f5864aa8221cb3acd2d9b97
Desire Barine <desirebarine16@gmail.com>
2022-12-13 09:17:42 +0100

    indentation fix

--- models/state.py
+++ models/state.py
@@ -6,7 +6,7 @@ from sqlalchemy.orm import relationship
 
 class State(BaseModel, Base):
     """ State class """
-        if models.storage_t == "db":
+    if models.storage_t == "db":
         __tablename__ = 'states'
         name = Column(String(128), nullable=False)
         cities = relationship("City", backref="state")
